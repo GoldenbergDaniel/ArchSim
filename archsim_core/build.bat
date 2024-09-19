8d0af9d269b388296de29c83d8fee580c4ce4013
GoldenbergDaniel <danielgoldenberg0@outlook.com>
2024-09-19 15:05:10 -0400

    Fix syntax for target selection

--- archsim_core/build.bat
+++ archsim_core/build.bat
@@ -9,8 +9,12 @@ if "%1%"=="d" set MODE=debug
 if "%1%"=="r" set MODE=release
 
 set TARGET=windows_amd64
-if "%1%"=="-target" set TARGET=%2%
-if "%2%"=="-target" set TARGET=%3%
+if "%1%"=="-target" (
+  set TARGET=%2%
+)
+if "%2%"=="-target" (
+  set TARGET=%3%
+)
 
 if "%MODE%"=="dev"     set FLAGS=-o:none -use-separate-modules
 if "%MODE%"=="debug"   set FLAGS=-o:none -debug
